d05993989aee74296b323da60d4dd23238e00428
Atlant <cuz.den30@gmail.com>
2022-12-14 22:29:28 +0300

    zadanie 56

--- zadanie 56/Program.cs
+++ zadanie 56/Program.cs
@@ -5,3 +5,61 @@
 // 8 4 2 4
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int[,] FillMatrix(int rows, int colums)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 6);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetSum(int[,] matrix)
+{
+    int str = 0;
+    int minSum = int.MaxValue;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        System.Console.WriteLine($"sum {sum} in {i + 1}");
+
+        if (minSum > sum)
+        {
+            minSum = sum;
+            str = i + 1;
+        }
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine($"min sum {minSum}  in {str}");
+
+}
+
+var arr = FillMatrix(5, 5);
+PrintMatrix(arr);
+System.Console.WriteLine();
+GetSum(arr);
+
