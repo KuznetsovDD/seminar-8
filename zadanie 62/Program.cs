8f1fec858cffda8edbabaae52090b82ab30d1e93
Atlant <cuz.den30@gmail.com>
2022-12-15 21:43:02 +0300

    zadanie 62

--- zadanie 62/Program.cs
+++ zadanie 62/Program.cs
@@ -4,3 +4,109 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+
+int[,] GetArray(int size)
+{
+    int[,] result = new int[size, size];
+    int i = 0;
+    int j = 0;
+    int rowE = size - 1;
+    int columnE = size - 1;
+    int rowS = 0;
+    int columnS = 0;
+    bool left = true;
+    bool top = true;
+    int count = 0;
+    while (count < size * size)
+    {
+        count++;
+        result[i, j] = count;
+        //идем вправо
+        if (left && top)
+        {
+            if (j == columnE)
+            {
+                rowS++;
+                top = true;
+                left = false;
+                i++;
+                continue;
+            }
+            else
+            {
+                j++;
+                continue;
+            }
+        }
+        //идем вниз
+        if (!left && top)
+        {
+            if (i == rowE)
+            {
+                columnE--;
+                top = false;
+                left = false;
+                j--;
+                continue;
+            }
+            else
+            {
+                i++;
+                continue;
+            }
+        }
+        //идем влево
+        if (!left && !top)
+        {
+            if (j == columnS)
+            {
+                rowE--;
+                top = false;
+                left = true;
+                i--;
+                continue;
+            }
+            else
+            {
+                j--;
+                continue;
+            }
+        }
+        //Идем вверх
+        if (left && !top)
+        {
+            if (i == rowS)
+            {
+                columnS++;
+                top = true;
+                left = true;
+                j++;
+                continue;
+            }
+            else
+            {
+                i--;
+                continue;
+            }
+        }
+
+    }
+    return result;
+}
+
+var i = GetArray(4);
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+PrintMatrix(i);
